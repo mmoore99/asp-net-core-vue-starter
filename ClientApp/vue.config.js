e8e9cafd001a5fd63405d93545467d291dcc3147
Mike Moore <mike@fiddlebits.net>
2020-02-27 09:49:38 -0800

    modified  vue.config.js to change build output directory

--- ClientApp/vue.config.js
+++ ClientApp/vue.config.js
@@ -1,8 +1,14 @@
 ﻿module.exports = {
+  outputDir: "../wwwroot/dist/",
+  filenameHashing: false,
+  devServer: {
+    //progress: false
+  },
+
   configureWebpack: {
     // Use source map for debugging in VS and VS Code
     // devtool: 'source-map',
-    // Breakpoints in VS and VSCode wont work since the source maps conside clien-app the project root, rather than its parent folder
+    // Breakpoints in VS and VSCode wont work since the source maps consider client-app the project root, rather than its parent folder
     output: {
       devtoolModuleFilenameTemplate: info => {
         const resourcePath = info.resourcePath.replace('./src', './ClientApp/src')
